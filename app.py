5d4e3fdb07c2b810b9e18e34a2b6721a626275bf
souravprakas-byte <souravprakas@gmail.com>
2026-05-04 12:11:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,4 @@ from odisha_ai_news.pipeline import main
 def run_pipeline():
     try:
         main()
-        return "Odisha News Intelligence Pipeline ran successfully ✅"
-    except Exception as e:
-        return f"Error running pipeline: {str(e)}"
-
-demo = gr.Interface(
-    fn=run_pipeline,
-    inputs=[],
-    outputs="text"
-)
-
-demo.launch()
+        return 
